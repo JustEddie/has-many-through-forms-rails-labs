76d2d44a06ea013b02c3c97b4eeb0bff566514a9
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-10-27 22:50:57 +1100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,20 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: ->(attributes) { attributes['name'].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :users, reject_if: ->(attributes) { attributes['username'].blank? }, allow_destroy: true
 
+  # def categories_attributes=(category_attributes)
+  #   category_attributes.values.each do |category_attribute|
+  #     category = Category.find_or_create_by(category_attribute)
+  #     categories << category
+  #   end
+  # end
 
+  # def users_attributes=(user_attributes)
+  #   user_attributes.values.each do |user_attribute|
+  #     user = User.find_or_create_by(user_attribute)
+  #     users << user
+  #   end
+  # end
 end
